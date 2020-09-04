54c2ba5c3c1eaadbb676619c091b40db42cd6760
vitaliejicol <vitaliejicol89@gmail.com>
2020-09-04 00:44:25 +0000

    change def environment

--- packer_json.groovy
+++ packer_json.groovy
@@ -1,36 +1,34 @@
-properties([
-    parameters([
-        choice(choices: ['dev', 'qa', 'prod'], description: '', name: 'environment')
-    ])
-])
-
 def aws_region_var = ''
+def environment = ''
 
-if(params.environment == "dev"){
+if(params.environment ==~ "dev-*"){
     aws_region_var = "us-east-1"
+    environment = 'dev'
 }
-else if(params.environment == "qa"){
+else if(params.environment ==~ "qa-*"){
     aws_region_var = "us-east-2"
+    environment = 'qa'
 }
-else if(params.environment == "prod"){
+else if(params.environment ==~ "prod-*"){
     aws_region_var = "us-west-2"
+    environment = 'prod'
 }
 
 node {
     stage('Pull Repo') {
-        git url: 'https://github.com/vitaliejicol/packer.works.git'
+        git url: 'https://github.com/ikambarov/packer.git'
     }
 
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(["AWS_REGION=${aws_region_var}", "PACKER_AMI_NAME=worker_prepare-${UUID.randomUUID().toString()}"]) {
+        withEnv(["AWS_REGION=${aws_region_var}", "PACKER_AMI_NAME=apache-${UUID.randomUUID().toString()}"]) {
             stage('Packer Validate') {
-                sh 'packer validate worker_prepare.json'
+                sh 'packer validate apache.json'
             }
 
             stage('Packer Build') {
-                sh 'packer build worker_prepare.json | tee output.txt'
+                sh 'packer build apache.json | tee output.txt'
 
-                def ami_id = sh(script: 'cat output.txt | grep ${aws_region_var}  | awk \'{print $2}\'', returnStdout: true)
+                def ami_id = sh(script: 'cat output.txt | grep us-east-2 | awk \'{print $2}\'', returnStdout: true)
                 println(ami_id)
             }
         }  
